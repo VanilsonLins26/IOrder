028da3ec632d40a996a6dc17c0fc8066f74f6ab0
vanilson <vanilson.liins@gmail.com>
2026-06-22 14:31:21 -0300

    feat: criação entidade loja e agregados

--- src/Backend/IOrder.Domain/Entities/Store.cs
+++ src/Backend/IOrder.Domain/Entities/Store.cs
@@ -4,6 +4,17 @@ using System.Text;
 
 namespace IOrder.Domain.Entities;
 
-internal class Store
+public class Store
 {
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Address { get; set; }
+    public DateTime EntryDate { get; set; } = DateTime.Now;
+    public string? About { get; set; }
+    public ICollection<Product> Products { get; set; } = [];
+    public ICollection<OpeningHour> OpeningHours { get; set; } = [];
+    public int CategoryId { get; set; }
+    public String UserId { get; set; }
+    //public Category? Category { get; set; }
+
 }
