97babe845fc5060537a71173fd44970fe3fb26df
vanilson <vanilson.liins@gmail.com>
2026-07-13 13:30:27 -0300

    test: ajusta e remove testes antigos vinculados ao MercadoPago

--- tests/CommomTestUtilities/Repositories/PaymentWriteOnlyRepositoryBuilder.cs
+++ tests/CommomTestUtilities/Repositories/PaymentWriteOnlyRepositoryBuilder.cs
@@ -13,29 +13,9 @@ public class PaymentWriteOnlyRepositoryBuilder
         _mock = new Mock<IPaymentWriteOnlyRepository>();
     }
 
-    public PaymentWriteOnlyRepositoryBuilder CreateAsync()
+    public void BuildGetByIdTracking(Payment payment)
     {
-        _mock.Setup(r => r.CreateAsync(It.IsAny<Payment>()))
-            .Returns(Task.CompletedTask);
-        return this;
-    }
-
-    public PaymentWriteOnlyRepositoryBuilder GetById(Payment? payment)
-    {
-        _mock.Setup(r => r.GetById(It.IsAny<Guid>())).ReturnsAsync(payment);
-        return this;
-    }
-
-    public PaymentWriteOnlyRepositoryBuilder GetByMercadoPagoId(Payment? payment)
-    {
-        _mock.Setup(r => r.GetByMercadoPagoId(It.IsAny<string>())).ReturnsAsync(payment);
-        return this;
-    }
-
-    public PaymentWriteOnlyRepositoryBuilder Update()
-    {
-        _mock.Setup(r => r.Update(It.IsAny<Payment>())).Returns((Payment p) => p);
-        return this;
+        _mock.Setup(x => x.GetByIdTracking(payment.Id)).ReturnsAsync(payment);
     }
 
     public IPaymentWriteOnlyRepository Build() => _mock.Object;
